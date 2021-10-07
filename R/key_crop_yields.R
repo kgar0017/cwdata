9411cb136937efeeb243a64286f6acaa0cf7c717
kgar0017 <kgar0017@student.monash.edu>
2021-10-07 15:11:39 +0530

    Added help file

---
+++ R/key_crop_yields.R
@@ -0,0 +1,30 @@
+
+#' Title
+#'
+#'
+#' More details aout the data
+#'
+#' @section  Data cotents
+#'
+#'  @format
+#'
+#'
+#' @source Where you get the data
+#' @examples
+#'
+#'
+#' A dataset containing 143825 observation and 5 variablesdevtools::document()
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{entity}{coutry name}
+#'   \item{code}{cpountry code}
+#'   \item{year}{year recorded}
+#'   \item{crop}{crop produces}
+#'   \item{tonnes_per_hectare}{tonnes per hectare produced}
+#'
+#'   ...
+#' }
+#'
+#' @source \url{https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv}
+"key_crop_yields"
